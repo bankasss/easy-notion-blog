d35f6d09efabd9b30b30e75a14c0a274e4b12fc0
bankasss <56781626+bankasss@users.noreply.github.com>
2022-12-31 14:39:27 +0900

    tes

--- app/blog/tag/[tag]/page.tsx
+++ app/blog/tag/[tag]/page.tsx
@@ -61,7 +61,7 @@ const BlogTagPage = async ({ params: { tag: encodedTag } }) => {
                   <PostTags post={post} />
                 </div>
                 <PostTitle post={post} />
-                <img src={NEXT_PUBLIC_URL && post.OGImage && new URL(`/api/og-image/${post.Slug}`, NEXT_PUBLIC_URL).toString()} alt="" />
+                <img src={new URL(`/api/og-image/${post.Slug}`).toString()} alt="" />
                 <PostExcerpt post={post} />
                 {/*<ReadMoreLink post={post} />*/}
                 <hr className="divider"></hr>
